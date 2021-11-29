f4d8c3196dbd0187af0ac6e3848520f5e89d2476
TimWhite <36320909+timwhitez@users.noreply.github.com>
2021-11-29 11:33:32 +0800

    Update main.go

--- example/main.go
+++ example/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/timwhitez/Doge-Gabh/pkg/Gabh"
 	"syscall"
+	"unsafe"
 )
 
 func main(){
@@ -17,7 +18,7 @@ func main(){
 
 	fmt.Printf("%s: %x\n",moduleN,sleep_ptr)
 	syscall.Syscall(uintptr(sleep_ptr),1,1000,0,0)
-	
+
 	//sha1(Sleep)=3cac34e674464c2b62286054cd9a2d2c81149efc
 	sleep_ptr,moduleN,err = gabh.GetFuncPtr(string([]byte{'k', 'e', 'r', 'n', 'e','l','3','2','.','d','l','l'}),"3cac34e674464c2b62286054cd9a2d2c81149efc",str2sha1)
 	if err != nil{
@@ -27,7 +28,22 @@ func main(){
 
 	fmt.Printf("%s: %x\n",moduleN,sleep_ptr)
 	syscall.Syscall(uintptr(sleep_ptr),1,1000,0,0)
+
+
+
+	//NtDelayExecution
+	sleep1,e := gabh.NtdllHgate("84804f99e2c7ab8aee611d256a085cf4879c4be8",str2sha1)
+	if e != nil {
+		panic(e)
+	}
+
+	fmt.Println(sleep1)
+	times := -(3000 * 10000)
+
+	//hellsgate syscall
+	gabh.HgSyscall(sleep1,0,uintptr(unsafe.Pointer(&times)))
 }
+
 
 
 func str2sha1(s string) string{
