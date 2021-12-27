78236ac0e77ff48b8d09379ac8cd3425af9c3691
TimWhite <36320909+timwhitez@users.noreply.github.com>
2021-12-27 17:00:13 +0800

    混淆go asm(add NOP)
    混淆go asm(add NOP)

--- pkg/Gabh/asm_x64.s
+++ pkg/Gabh/asm_x64.s
@@ -2,47 +2,73 @@
 #define maxargs 16
 //func Syscall(callid uint16, argh ...uintptr) (uint32, error)
 TEXT ·hgSyscall(SB), $0-56
+    NOP
 	XORQ AX,AX
+	NOP
 	MOVW callid+0(FP), AX
+	NOP
 	PUSHQ CX
+	NOP
 	//put variadic size into CX
 	MOVQ argh_len+16(FP),CX
+	NOP
 	//put variadic pointer into SI
 	MOVQ argh_base+8(FP),SI
+	NOP
 	// SetLastError(0).
 	MOVQ	0x30(GS), DI
+	NOP
 	MOVL	$0, 0x68(DI)
+	NOP
 	SUBQ	$(maxargs*8), SP	// room for args
+	NOP
 	// Fast version, do not store args on the stack.
 	CMPL	CX, $4
+	NOP
 	JLE	loadregs
 	// Check we have enough room for args.
 	CMPL	CX, $maxargs
+	NOP
 	JLE	2(PC)
 	INT	$3			// not enough room -> crash
+    NOP
 	// Copy args to the stack.
 	MOVQ	SP, DI
+	NOP
 	CLD
+	NOP
 	REP; MOVSQ
+	NOP
 	MOVQ	SP, SI
+	NOP
 	//move the stack pointer????? why????
 	SUBQ	$8, SP
+	NOP
 loadregs:
 	// Load first 4 args into correspondent registers.
 	MOVQ	0(SI), CX
+	NOP
 	MOVQ	8(SI), DX
+	NOP
 	MOVQ	16(SI), R8
+	NOP
 	MOVQ	24(SI), R9
+	NOP
 	// Floating point arguments are passed in the XMM
 	// registers. Set them here in case any of the arguments
 	// are floating point values. For details see
 	//	https://msdn.microsoft.com/en-us/library/zthk2dkh.aspx
 	MOVQ	CX, X0
+	NOP
 	MOVQ	DX, X1
+	NOP
 	MOVQ	R8, X2
+	NOP
 	MOVQ	R9, X3
+	NOP
 	//MOVW callid+0(FP), AX
 	MOVQ CX, R10
+	NOP
 	SYSCALL
 	ADDQ	$((maxargs+1)*8), SP
 	// Return result.
