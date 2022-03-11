486f3a8e34be0db1b1f51e77823e992d93b9d7a3
TimWhite <36320909+timwhitez@users.noreply.github.com>
2022-03-11 14:33:34 +0800

    add RecycledGate
    add RecycledGate

--- pkg/Gabh/asm_x64.s
+++ pkg/Gabh/asm_x64.s
@@ -1,6 +1,5 @@
 //based on https://golang.org/src/runtime/sys_windows_amd64.s
 #define maxargs 18
-
 //func Syscall(callid uint16, argh ...uintptr) (uint32, error)
 TEXT ·hgSyscall(SB), $0-56
 	BYTE $0x90			//NOP
@@ -80,6 +79,105 @@ loadregs:
 	POPQ	CX
 	MOVL	AX, errcode+32(FP)
 	RET
+
+
+//func reCycall(callid uintptr,syscallA uintptr,argh ...uintptr) (uint32, error)
+TEXT ·reCycall(SB),$0-56
+    PUSHQ CX
+	BYTE $0x90			//NOP
+	XORQ AX,AX
+	BYTE $0x90			//NOP
+	MOVW callid+0(FP), AX
+
+	BYTE $0x90			//NOP
+
+    XORQ R15,R15
+    MOVQ syscallA+8(FP), R15
+
+	BYTE $0x90			//NOP
+	//put variadic size into CX
+	MOVQ argh_len+24(FP),CX
+	BYTE $0x90			//NOP
+	//put variadic pointer into SI
+	MOVQ argh_base+16(FP),SI
+	BYTE $0x90			//NOP
+
+
+	// SetLastError(0).
+	MOVQ	0x30(GS), DI
+	BYTE $0x90			//NOP
+	MOVL	$0, 0x68(DI)
+	BYTE $0x90			//NOP
+
+
+	SUBQ	$(maxargs*8), SP	// room for args
+	BYTE $0x90			//NOP
+
+	// Fast version, do not store args on the stack.
+	CMPL	CX, $4
+	BYTE $0x90			//NOP
+	JLE	loadregs
+
+	// Check we have enough room for args.
+	CMPL	CX, $maxargs
+	BYTE $0x90			//NOP
+	JLE	2(PC)
+	INT	$3			// not enough room -> crash
+	BYTE $0x90			//NOP
+
+
+	// Copy args to the stack.
+	MOVQ	SP, DI
+	BYTE $0x90			//NOP
+	CLD
+	BYTE $0x90			//NOP
+	REP; MOVSQ
+	BYTE $0x90			//NOP
+	MOVQ	SP, SI
+	BYTE $0x90			//NOP
+loadregs:
+    //move the stack pointer????? why????
+	//SUBQ	$8, SP
+
+	// Load first 4 args into correspondent registers.
+	MOVQ	0(SI), CX
+	BYTE $0x90			//NOP
+	MOVQ	8(SI), DX
+	BYTE $0x90			//NOP
+	MOVQ	16(SI), R8
+	BYTE $0x90			//NOP
+	MOVQ	24(SI), R9
+	BYTE $0x90			//NOP
+	// Floating point arguments are passed in the XMM
+	// registers. Set them here in case any of the arguments
+	// are floating point values. For details see
+	//	https://msdn.microsoft.com/en-us/library/zthk2dkh.aspx
+	MOVQ	CX, X0
+	BYTE $0x90			//NOP
+	MOVQ	DX, X1
+	BYTE $0x90			//NOP
+	MOVQ	R8, X2
+	BYTE $0x90			//NOP
+	MOVQ	R9, X3
+	BYTE $0x90			//NOP
+
+    //mov r10, rax
+	MOVQ CX, R10
+	BYTE $0x90			//NOP
+
+    //syscall;ret
+    CALL R15
+
+    BYTE $0x90			//NOP
+
+    ADDQ	$((maxargs)*8), SP
+
+    // Return result.
+    POPQ	CX
+    MOVL	AX, errcode+40(FP)
+
+    RET
+
 
 
 //func eggCall(callid uint16, argh ...uintptr) (uint32, error)
@@ -223,4 +321,5 @@ endloop:
 	MOVQ CX, modulepath+24(FP)
 	//SYSCALL
 	RET
+
 
