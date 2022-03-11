0d438e7a8d59f20024c7eb4cde79c9660cf3739d
TimWhite <36320909+timwhitez@users.noreply.github.com>
2022-03-11 14:33:12 +0800

    Update gabh.go

--- pkg/Gabh/gabh.go
+++ pkg/Gabh/gabh.go
@@ -21,6 +21,118 @@ import (
 	"golang.org/x/sys/windows"
 )
 
+
+func contains(slice []string, item string) bool {
+	set := make(map[string]struct{}, len(slice))
+	for _, s := range slice {
+		set[s] = struct{}{}
+	}
+
+	_, ok := set[item]
+	return ok
+}
+
+
+func GetRecyCall(tarApi string,blacklist []string,hash func(string) string) uintptr {
+	//init hasher
+	hasher := func(a string)string{
+		return a
+	}
+	if hash !=nil{
+		hasher = hash
+	}
+
+	//tolower
+	if blacklist != nil && tarApi == ""{
+		for i,v := range blacklist{
+			blacklist[i] = strings.ToLower(v)
+		}
+	}
+
+
+	Ntd, _, _ := gMLO(1)
+	if Ntd == 0 {
+		return 0
+	}
+
+	//fmt.Printf("NtdllBaseAddr: 0x%x\n", Ntd)
+
+	addrMod := Ntd
+
+	ntHeader := ntH(addrMod)
+	if ntHeader == nil {
+		return 0
+	}
+	//windows.SleepEx(50, false)
+	//get module size of ntdll
+	modSize := ntHeader.OptionalHeader.SizeOfImage
+	if modSize == 0 {
+		return 0
+	}
+
+	rr := rawreader.New(addrMod, int(modSize))
+	p, e := pe.NewFileFromMemory(rr)
+	if e != nil {
+		return 0
+	}
+	ex, e := p.Exports()
+
+	rand.Seed(time.Now().UnixNano())
+	for i := range ex {
+		j := rand.Intn(i + 1)
+		ex[i], ex[j] = ex[j], ex[i]
+	}
+
+	for i := 0; i < len(ex); i++ {
+		exp := ex[i]
+		if tarApi != ""{
+			if strings.ToLower(hasher(exp.Name)) == strings.ToLower(tarApi)||strings.ToLower(hasher(strings.ToLower(exp.Name))) == strings.ToLower(tarApi) {
+				//fmt.Println("Syscall API: " + exp.Name)
+				offset := rvaToOffset(p, exp.VirtualAddress)
+				b, e := p.Bytes()
+				if e != nil {
+					return 0
+				}
+				buff := b[offset : offset+32]
+				if buff[18] == 0x0f && buff[19] == 0x05 && buff[20] == 0xc3 {
+					//fmt.Printf("Syscall;ret Address: 0x%x\n", Ntd+uintptr(exp.VirtualAddress)+uintptr(18))
+					return Ntd + uintptr(exp.VirtualAddress) + uintptr(18)
+				}
+			}
+		}else {
+			if strings.HasPrefix(exp.Name, "Nt") || strings.HasPrefix(exp.Name, "Zw"){
+				if !contains(blacklist, strings.ToLower(hasher(exp.Name))) && !contains(blacklist, strings.ToLower(hasher(strings.ToLower(exp.Name)))) {
+					//fmt.Println("Syscall API: " + exp.Name)
+					offset := rvaToOffset(p, exp.VirtualAddress)
+					b, e := p.Bytes()
+					if e != nil {
+						return 0
+					}
+					buff := b[offset : offset+32]
+					if buff[18] == 0x0f && buff[19] == 0x05 && buff[20] == 0xc3 {
+						//fmt.Printf("Syscall;ret Address: 0x%x\n", Ntd+uintptr(exp.VirtualAddress)+uintptr(18))
+						return Ntd + uintptr(exp.VirtualAddress) + uintptr(18)
+					}
+				}
+			}
+		}
+	}
+	return 0
+}
+
+//ReCycall calls the system function specified by callid with n arguments. Works much the same as syscall.Syscall - return value is the call error code and optional error text. All args are uintptrs to make it easy.
+func ReCycall(callid uint16, syscallA uintptr, argh ...uintptr) (errcode uint32, err error) {
+
+	errcode = reCycall(callid, syscallA, argh...)
+
+	if errcode != 0 {
+		err = fmt.Errorf("non-zero return from syscall")
+	}
+	return errcode, err
+}
+
+
+
 type SPFG struct {
 	Fakename string
 	Pointer  uintptr
@@ -989,10 +1101,15 @@ func (s sstring) String() string {
 }
 
 
+//reCycall calls the system function specified by callid with n arguments. Works much the same as syscall.Syscall - return value is the call error code and optional error text. All args are uintptrs to make it easy.
+func reCycall(callid uint16, syscallA uintptr, argh ...uintptr) (errcode uint32)
+
+
 func eggCall(callid uint16, argh ...uintptr) (errcode uint32)
 
 //Syscall calls the system function specified by callid with n arguments. Works much the same as syscall.Syscall - return value is the call error code and optional error text. All args are uintptrs to make it easy.
 func hgSyscall(callid uint16, argh ...uintptr) (errcode uint32)
+
 
 //getModuleLoadedOrder returns the start address of module located at i in the load order. This might be useful if there is a function you need that isn't in ntdll, or if some rude individual has loaded themselves before ntdll.
 func getMLO(i int) (start uintptr, size uintptr, modulepath *sstring)
